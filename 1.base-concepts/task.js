e95446cc9970fda679d17de77370fe5d7d3e2422
Екатерина Слюсар <Ekaterina_sasha_14@mail.ru>
2022-06-07 15:49:43 +0300

    Добавлено решение первой задачи в task.js

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,13 +1,34 @@
+"use strict"
+
 function solveEquation(a, b, c) {
   let arr;
-  // код для задачи №1 писать здесь
+  let d = b**(2)-4*a*c;
+
+  if (d < 0) {
+    arr = []; //корней нет;
+  }
+
+  if (d === 0 ) {
+    arr = [(-b/(2*a))]; //один корень;
+  }
+
+  if ( d > 0) {
+    arr = [
+      (-b + Math.sqrt(d) )/(2*a), //первый корень;
+      (-b - Math.sqrt(d) )/(2*a) // второй корень;
+    ]    
+}
   return arr; // array
 }
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
-  let totalAmount;
+  let totalAmount = amount - contribution;
+  const date = new Date();
+  console.log(Number(percent)); 
 
-  // код для задачи №2 писать здесь
+ 
 
+  // console.log(totalAmount.toFixed(2)); //округление до 2 цифр после запятой
+  
   return totalAmount;
 }
