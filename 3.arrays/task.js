ddde7d1e6acf31cbb25ff13ed91d3003a9a71342
Екатерина Слюсар <Ekaterina_sasha_14@mail.ru>
2022-06-16 00:00:14 +0300

    Добавлено решение задач по теме Работа с массивами

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,18 @@
 function compareArrays(arr1, arr2) {
   let result;
-
-  // Ваш код
-
-  return result; // boolean
+    if (arr1.length === arr2.length && arr1.every((n, i) => n === arr2[i])) { 
+      result = true;
+  }
+    else {
+      result = false;
+  }
+  return result ;
 }
 
+
+// const compareArrays = (arr1, arr2) =>  arr1.length === arr2.length  && arr1.every((n, i) => n === arr2[i]); второй вариант
+
 function advancedFilter(arr) {
-  let resultArr;
-
-  // Ваш код
-
+  let resultArr = arr.filter(arr => arr > 0).filter(arr => arr % 3 === 0).map(arr => arr*10);
   return resultArr; // array
 }
