6f3f4956cc028430bddb4c9a19571e9e7d28c33b
Екатерина Слюсар <Ekaterina_sasha_14@mail.ru>
2022-06-19 11:15:58 +0300

    Добавлено решение задачи по теме Объекты

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,51 @@
+
 function Student(name, gender, age) {
-    // Ваш код
-
+  Object.assign(this, {name, gender, age});
 }
 
+  const student1 = new Student("Artem", "male", 18);
+  const student2 = new Student("Ann", "female", 20);
+  
+//Добавление предмета
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+ this.subject = subjectName;
 }
 
-// ваш код для остальных методов
+// Добавление оценки + проверка наличия свойства marks
+Student.prototype.addMark = function (mark) {
+  
+  if(this.marks === undefined){ 
+    this.marks =[mark]; 
+    } 
+  else {
+    this.marks.push(mark) ;
+    }
+}
+
+//Добавление нескольких отметок одновременно
+Student.prototype.addMarks = function (...mark){
+  if(this.marks === undefined){ 
+    this.marks = [];   
+}
+  for (let marks of mark){
+      this.marks.push(marks) 
+    } 
+}
+
+//Вычисление среднего арифметического
+Student.prototype.getAverage = function() {
+  const average = this.marks.reduce((acc, mark) => {      
+    return acc + mark;  
+});
+return (average/this.marks.length);
+}
+
+// Удаление свойства/ отчисление
+Student.prototype.exclude = function(reason) {
+  delete Student.prototype.subject; 
+  delete Student.prototype.marks;
+  this.excluded = reason;
+}
+
+
+
