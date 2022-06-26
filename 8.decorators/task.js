520ba8c47b82f9d05238cf9327c295b5d5efaa91
Екатерина Слюсар <Ekaterina_sasha_14@mail.ru>
2022-06-26 22:42:53 +0300

    Добавлено решение задач по теме Декораторы

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,12 +1,46 @@
 function cachingDecoratorNew(func) {
-  // Ваш код
+  let cache = [];
+    function wrapper(...args) {
+      const hash = args.join(',');
+      let objectInCache = cache.findIndex((item) => item.hash == hash); 
+      if (objectInCache !== -1) { 
+          console.log("Из кэша: " + cache[objectInCache].result);
+          return "Из кэша: " + cache[objectInCache].result;
+      }
+
+      let result = func(...args); 
+      cache.push({hash, result}) ; 
+      if (cache.length > 5) { 
+        cache.shift();
+      }
+      console.log("Вычисляем: " + result);
+      return "Вычисляем: " + result;  
+  }
+  return wrapper;
 }
 
-
-function debounceDecoratorNew(func) {
-  // Ваш код
+function debounceDecoratorNew(func, ms) {
+  let timerId = null;  
+    function wrapper(...args) {
+      if (timerId === null) {
+        func(...args);
+      }
+      clearTimeout(timerId);
+      timerId = setTimeout(() => func(...args), ms);
+    }
+    return wrapper;
 }
 
-function debounceDecorator2(func) {
-  // Ваш код
+function debounceDecorator2(func, ms) {
+  let timerId = null;
+    function wrapper(...args) {
+      if (timerId === null) {
+        func(...args);
+      }
+      clearTimeout(timerId);
+      timerId = setTimeout(() => func(...args), ms);
+      wrapper.count++;
+    }
+    wrapper.count = 0;
+    return wrapper;
 }
